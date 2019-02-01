41983f82a2bd0b4690d90512516a7eaad1c8eb54
mmtrn <mmtrn@outlook.com>
2019-02-01 19:00:22 +0300

    Tez

---
+++ Bitirme Tezi/201313172050 Mehmet Turan/Client/Form1.Designer.cs
@@ -0,0 +1,223 @@
+﻿namespace Client
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnBaglan = new System.Windows.Forms.Button();
+            this.LVMess = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.txtMesaj = new System.Windows.Forms.TextBox();
+            this.txtRenk = new System.Windows.Forms.TextBox();
+            this.txtNickName = new System.Windows.Forms.TextBox();
+            this.txtSunucu = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Location = new System.Drawing.Point(383, 81);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(273, 286);
+            this.panel1.TabIndex = 16;
+            this.panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.basla);
+            this.panel1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.devam);
+            this.panel1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.bitir);
+            // 
+            // btnBaglan
+            // 
+            this.btnBaglan.Location = new System.Drawing.Point(581, 6);
+            this.btnBaglan.Name = "btnBaglan";
+            this.btnBaglan.Size = new System.Drawing.Size(75, 56);
+            this.btnBaglan.TabIndex = 15;
+            this.btnBaglan.Text = "Baglan";
+            this.btnBaglan.UseVisualStyleBackColor = true;
+            this.btnBaglan.Click += new System.EventHandler(this.btnBaglan_Click);
+            // 
+            // LVMess
+            // 
+            this.LVMess.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2});
+            this.LVMess.Location = new System.Drawing.Point(87, 81);
+            this.LVMess.Name = "LVMess";
+            this.LVMess.Size = new System.Drawing.Size(290, 251);
+            this.LVMess.TabIndex = 14;
+            this.LVMess.UseCompatibleStateImageBehavior = false;
+            this.LVMess.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Nick";
+            this.columnHeader1.Width = 84;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Mesaj";
+            this.columnHeader2.Width = 202;
+            // 
+            // txtMesaj
+            // 
+            this.txtMesaj.Location = new System.Drawing.Point(87, 347);
+            this.txtMesaj.Name = "txtMesaj";
+            this.txtMesaj.Size = new System.Drawing.Size(290, 20);
+            this.txtMesaj.TabIndex = 11;
+            this.txtMesaj.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtMesaj_KeyDown);
+            // 
+            // txtRenk
+            // 
+            this.txtRenk.Location = new System.Drawing.Point(465, 25);
+            this.txtRenk.Name = "txtRenk";
+            this.txtRenk.Size = new System.Drawing.Size(100, 20);
+            this.txtRenk.TabIndex = 12;
+            this.txtRenk.Text = "Red";
+            // 
+            // txtNickName
+            // 
+            this.txtNickName.Location = new System.Drawing.Point(87, 42);
+            this.txtNickName.Name = "txtNickName";
+            this.txtNickName.Size = new System.Drawing.Size(124, 20);
+            this.txtNickName.TabIndex = 13;
+            this.txtNickName.Text = "Kullanici";
+            // 
+            // txtSunucu
+            // 
+            this.txtSunucu.Location = new System.Drawing.Point(87, 6);
+            this.txtSunucu.Name = "txtSunucu";
+            this.txtSunucu.Size = new System.Drawing.Size(100, 20);
+            this.txtSunucu.TabIndex = 10;
+            this.txtSunucu.Text = "localhost";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 350);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(35, 13);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Mesaj";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 81);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(46, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Mesajlar";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(409, 28);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(33, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Renk";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 45);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Nick";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Sunucu";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(465, 52);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 17;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(668, 376);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.btnBaglan);
+            this.Controls.Add(this.LVMess);
+            this.Controls.Add(this.txtMesaj);
+            this.Controls.Add(this.txtRenk);
+            this.Controls.Add(this.txtNickName);
+            this.Controls.Add(this.txtSunucu);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnBaglan;
+        private System.Windows.Forms.ListView LVMess;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.TextBox txtMesaj;
+        private System.Windows.Forms.TextBox txtRenk;
+        private System.Windows.Forms.TextBox txtNickName;
+        private System.Windows.Forms.TextBox txtSunucu;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Bitirme Tezi/201313172050 Mehmet Turan/Client/Form1.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using RemoteObject;
+using System.Runtime.Remoting.Channels;
+using System.Collections;
+using System.Runtime.Remoting.Channels.Tcp;
+using System.Runtime.Remoting;
+
+namespace Client
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            CheckForIllegalCrossThreadCalls = false;
+        }
+
+        messObject mess;
+        remotObject remo;
+        alertObject alert;
+        drawObject draw;
+
+        private void btnBaglan_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start(Application.StartupPath + @"\Sunucu.exe");
+           
+
+//MessageBox.Show(Application.StartupPath+@"\dosyaismi.ini");
+
+            BinaryClientFormatterSinkProvider cli = new
+            BinaryClientFormatterSinkProvider();
+            BinaryServerFormatterSinkProvider slv = new
+            BinaryServerFormatterSinkProvider();
+            slv.TypeFilterLevel = System.Runtime.Serialization.Formatters.TypeFilterLevel.Full;
+            Hashtable ht = new Hashtable();
+            ht["port"] = 0;
+            TcpChannel c = new TcpChannel(ht, cli, slv);
+            ChannelServices.RegisterChannel(c, false);
+            RemotingConfiguration.RegisterWellKnownClientType(typeof(remotObject),
+                "tcp://" + txtSunucu.Text.Trim() + ":1797/chatUri");
+            mess = new messObject();
+            remo = new remotObject();
+            alert = new alertObject();
+            draw = new drawObject();
+
+            remo.messEvent += new messDelegate(mess.SendMessage);
+            remo.alertEvent += new alertDelegate(alert.alert);
+            remo.drawEvent += new drawDelegate(draw.draw);
+
+            mess.messEvent += new messDelegate(mess_messEvent);
+            alert.alertEvent += new alertDelegate(alert_alertEvent);
+            draw.drawEvent += new drawDelegate(draw_drawEvent);
+            this.Text = remo.getMachineName();
+            remo.alert(txtNickName.Text);
+        }
+
+        void draw_drawEvent(int x, int y, Color renk)
+        {
+            Graphics g = panel1.CreateGraphics();
+            SolidBrush bru = new SolidBrush(renk);
+            g.FillEllipse(bru, x, y, 10, 10);
+        }
+        void alert_alertEvent(string kisi)
+        {
+            if (kisi != txtNickName.Text.Trim())
+            {
+                MessageBox.Show(kisi + "baglandi");
+            }
+        }
+        void mess_messEvent(string text)
+        {
+            ListViewItem li = new ListViewItem();
+            li.Text = text.Split(',')[0];
+            li.SubItems.Add(text.Split(',')[1]);
+            LVMess.Items.Add(li);
+        }
+
+        private void txtMesaj_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                remo.SendMessage(txtNickName.Text + "," + txtMesaj.Text);
+                txtMesaj.Clear();
+                txtMesaj.Focus();
+            }
+        }
+
+        bool durum;
+
+        private void basla(object sender, MouseEventArgs e)
+        {
+            durum = true;
+        }
+
+        private void devam(object sender, MouseEventArgs e)
+        {
+            if (durum)
+            {
+                Graphics g = panel1.CreateGraphics();
+                Color renk = Color.FromName(txtRenk.Text.Trim());
+                SolidBrush b = new SolidBrush(renk);
+                g.FillEllipse(b, e.X, e.Y, 10, 10);
+                remo.draw(e.X, e.Y, renk);
+
+            }
+        }
+        private void bitir(object sender, MouseEventArgs e)
+        {
+            durum = false;
+        }
+
+       
+
+
+
+
+
+    }
+}
